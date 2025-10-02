024f09dfe72bebaa864fa643f8e3f91191c5f858
fauziramadhani <93056712+fauziramadha@users.noreply.github.com>
2025-10-03 02:30:12 +0700

    Update vite.config.js

--- shop/vite.config.js
+++ shop/vite.config.js
@@ -3,7 +3,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  root: '.',              // root project (default)
+  publicDir: 'public',    // supaya folder public/assets bisa kebaca
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    emptyOutDir: true     // kosongkan dist setiap build
+  },
+  server: {
+    port: 5173,
+    open: true
   }
 })
