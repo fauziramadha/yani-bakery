27a554033d62247abc48514a9af3239514ffbbaf
fauziramadhani <93056712+fauziramadha@users.noreply.github.com>
2025-10-03 02:52:58 +0700

    Update vite.config.js

--- shop/vite.config.js
+++ shop/vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: '.',              // root project (default)
-  publicDir: 'public',    // supaya folder public/assets bisa kebaca
+  root: '.',              // root project
+  publicDir: 'public',    // folder public/assets
+  base: '/',              // penting supaya file JS/CSS dimuat di Vercel
   build: {
-    outDir: 'dist',
+    outDir: 'dist',       // hasil build masuk dist/
     emptyOutDir: true     // kosongkan dist setiap build
   },
   server: {
